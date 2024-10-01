90530069db04eb721b5461d7d0bf580bf5fae34b
suman154 <sumanbhatta735@gmail.com>
2024-10-01 22:45:32 +0545

    model register in admin

--- job_portal/jobs/admin.py
+++ job_portal/jobs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Job, Company, Application
 # Register your models here.
+
+admin.site.register(Job)
+admin.site.register(Company)
+admin.site.register(Application)
